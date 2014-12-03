1a5b130cdde0f1af0197d112a8972b377e78798e
Doron Rainer <doron500@gmail.com>
2014-12-03 19:31:58 +0200

    Make it more clean, and bugs fixed! Safe to use

--- adbDB.py
+++ adbDB.py
@@ -1,77 +1,148 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-	
-import os, platform, re
+import os, platform, re, sys
 from subprocess import PIPE, Popen
 
 SDK_PATH = "/opt/android-sdk/"
 OS = platform.system()
 
+
+# Command line to control adb
 def cmdline(command):
 	process = Popen(args=command, stdout=PIPE, shell=True)
-	return process.communicate()[0].decode("ASCII")
+	return process.communicate()[0].decode("UTF-8")
+	
+	
+# Get some string from cmd result and make a clean array
+def makeCleanArray(text,splitCndition, start=0, replaces=[]):
+	
+	text = text.rstrip()
+	if (len(replaces) > 0):
+		for rl in replaces:
+			text = text.replace(rl, "")
+	output = text.split(splitCndition)
+	
+	return output[start:]
 
-if __name__=="__main__":
-	deviceList = []
-	if (os.path.exists(SDK_PATH + "platform-tools/")):
-		platfromtools = SDK_PATH + "platform-tools/"
-		if (OS == 'Windows'):
-			adb = platfromtools + "adb.exe"
-			sqlite = platfromtools + "sqlite3.exe"
-		else:
-			adb = platfromtools + "adb"
-			sqlite = platfromtools + "sqlite3"
+# Ask the user in which device to use
+def selectDevice():
+	devices = makeCleanArray(cmdline("%s devices" % adb),"\n",1,[" ", "\tdevice","\r"])
+	userInput = 0
+	if (len(devices) == 0):
+		print("No available devices")
+		sys.exit(1)
+	
+	print("# Please select a device:")
+	i=1
+	for device in devices:
+		model = cmdline("%s -s %s shell getprop ro.product.model" % (adb, device))
+		print("%d) %s - %s" % (i, device, model.rstrip("\n")))
+		i = i + 1
+	while (True):
+		userInput = input(">> ")
+		try:
+			userInput = int(userInput)
+			if (userInput > len(devices)):
+				print("Device doesn't exists, try again")
+			else:
+				return devices[userInput-1]
 				
-		cmdline("%s start-server" % adb)
-		devices = cmdline(adb + " devices").split("\n")[1:]
-		devices = [device for device in devices if device != '']
+		except:
+			print("Inavlid input, please insert a valid number!")
 		
-		print("Please select a device:\n")
-		i=1
-		for device in devices:
-			curDevice = device.split("\t")[0]
-			model = cmdline('%s -s %s shell "getprop ro.product.model"' % (adb, curDevice))
-			deviceList.append(curDevice)
-			print("%d) %s - %s" % (i, curDevice, model.rstrip("\n")))
+		
+# Ask the user which package to use
+def selectPackage(device):
+	packages = cmdline("%s -s %s shell pm list packages -3 -i" % (adb, device))
+	packages = makeCleanArray(packages, "\n", 0, ["\r",""])
+	newPackages = []
+	if (len(packages) == 0):
+		print("No available packages")
+		sys.exit(1)
+	
+	print("# Please select a package:")
+	i=1
+	for package in packages:
+		if(package.find("installer=null") != -1):
+			newPackages.append(package.replace("installer=null", "").replace(" ", "").replace("package:",""))
+			print("%d) %s" % (i, newPackages[i-1]))
 			i = i + 1
+	while(True):
+		userInput = input(">> ")
+		try:
+			userInput = int(userInput)
+			if (userInput > len(newPackages)):
+				print("Package doesn't exists, try again")
+			else:
+				return newPackages[userInput-1]
+				
+		except:
+			print("Inavlid input, please insert a valid number!")
+
+
+# Ask the user which file to select
+def selectFile(device, package):
+	files =	makeCleanArray(cmdline("%s -s %s shell run-as %s ls databases/" % (adb, device, package)), "\n",0,["","\r"])
+	if (len(files) == 1 and files[0] == ""):
+		print("No available files")
+		sys.exit(1)
+		
+	newFiles = []
+	print("# Please select a file:")
+	i=1
+	for fl in files:
+		if (fl.find(".db-journal") == -1):
+			newFiles.append(fl)
+			print("%d) %s" %(i, fl))
+			i = i +1
 			
-			
-		userInput = int(input("Enter number of device: "))
-		selectedDevice = deviceList[userInput - 1]
+	while(True):
+		userInput = input(">> ")
+		try:
+			userInput = int(userInput)
+			if (userInput > len(newFiles)):
+				print("File doesn't exists, try again")
+			else:
+				return newFiles[userInput-1]
+				
+		except:
+			print("Inavlid input, please insert a valid number!")
+	
+	
+	
+if __name__=="__main__":
+	flag = True
+	if (os.path.exists(SDK_PATH + "platform-tools/")):
+		adb = SDK_PATH + "platform-tools/adb"
+		if (OS == 'Windows'):
+			adb = adb + ".exe"
+	
+		cmdline("%s start-server" % adb)
 		
-		packageList = cmdline("%s -s %s shell pm list packages -3 -i | grep null | sed 's/package://' | sed 's/installer=null//'" % (adb, selectedDevice)).strip().replace("\r","").replace(" ", "").split("\n")
-		print("Please select a package: ")
-		i = 1
-		for pk in packageList:
-			print("%d) %s" % (i, pk))
-			i = i + 1 
-			
-		package = packageList[int(input("Enter number of package: ")) - 1]
-		print(package)
+		selectedDevice  = selectDevice()
+		selectedPackage = selectPackage(selectedDevice)
+		selectedFile = selectFile(selectedDevice, selectedPackage)
 		
-		listFiles = cmdline("%s -s %s shell run-as %s ls databases/" % (adb, selectedDevice, package)).rstrip("\n").replace("\r", "").split("\n")
+		print("# Press '1' to get the file")
+		print("# Press '2' to remove it from device")
 		
-		print("Select file: ");
-		i=1
-		for fl in listFiles:
-			if (fl.find("-journal") == -1):
-				print("%d) %s" % (i, fl))
-				i = i + 1
-			
-		userInput = int(input("Enter number of file: "))
-		selectedFile = listFiles[userInput - 1]
-		
-		print("To get the file press '1', to remove it press '2'")
-		userInput = int(input("number: "))
-		if (userInput == 1):
-			cmd = "{0} -s {1} shell run-as {2} cp databases/{3} /sdcard/".format(adb, selectedDevice, package, selectedFile)
-			cmdline(cmd)
-			cmdline("%s -s %s pull /sdcard/%s" % (adb, selectedDevice, selectedFile))
-			cmdline("%s -s %s shell rm /sdcard/%s" % (adb, selectedDevice, selectedFile))
-			print("Done!, saved as: %s/%s" % (cmdline("pwd").replace("\n",""),selectedFile))
-		else:
-			cmd = "{0} -s {1} shell run-as {2} rm databases/{3} databases/sdcard{3}-journal".format(adb, selectedDevice, package, selectedFile)
-			cmdline(cmd)
-			print("Removed!")
-		
+		while(flag):
+			userInput = input(">> ")
+			try:
+				userInput = int(userInput)
+				if (userInput == 1): 
+					cmdline("{0} -s {1} shell run-as {2} cp databases/{3} /sdcard/".format(adb, selectedDevice, selectedPackage, selectedFile))
+					cmdline("%s -s %s pull /sdcard/%s" % (adb, selectedDevice, selectedFile))
+					cmdline("%s -s %s shell rm /sdcard/%s" % (adb, selectedDevice, selectedFile))
+					flag = False
+					print("Downloaded!, saved as: %s/%s" % (cmdline("pwd").replace("\n",""),selectedFile))
+				elif (userInput == 2):
+					cmdline("{0} -s {1} shell run-as {2} rm databases/{3} databases/{3}-journal".format(adb, selectedDevice, selectedPackage, selectedFile))
+					flag = False
+					print("Removed!")
+				else:
+					print("What?! try again")
+			except:
+				print("Inavlid input, please insert a valid number!")
 	else:
 		print("Please launch the 'Android SDK Manager' and download Platform Tools!")
