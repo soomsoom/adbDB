d05ced53dec0f55076282fc3d423966a297fa1f4
Doron Rainer <doron500@gmail.com>
2014-12-06 18:10:30 +0200

    Making the tool more sopisticated!
    > Will select automaticlly device,package,file if there is only one item
    > Will never exit the program even if you finish an action, he will wait for
    the next action!
    > Now if you don't put '/' at the end of the SDK_PATH it will complete it!

--- adbDB.py
+++ adbDB.py
@@ -8,14 +8,11 @@ SDK_PATH = "/opt/android-sdk/"
 OS = platform.system()
 
 # Makeing widows users will see the messages!
-def doExit():
-	if (OS == 'Windows'):
-		c = "1"
-		while (c == "1"):
-			c = input("Press ENTER to continue...")
-		raise SystemExit
-		
-	else:
+def doExit(exit=1):
+	c = "1"
+	while (c == "1"):
+		c = input("Press ENTER to continue...")
+	if(exit == 1):
 		raise SystemExit
 		
 		
@@ -45,7 +42,12 @@ def selectDevice():
 	if (len(devices) == 0):
 		print("No available devices")
 		doExit()
-	
+		
+	if (len(devices) == 1):
+		model = cmdline("%s -s %s shell getprop ro.product.model" % (adb, devices[0])).rstrip("\n").rstrip("\r")
+		print("## Device %s - %s selected! ##" % (devices[0], model))
+		return devices[0]
+		
 	print("# Please select a device:")
 	i=1
 	for device in devices:
@@ -81,6 +83,10 @@ def selectPackage(device):
 		print("No available packages")
 		doExit()
 	
+	if (len(packages) == 1):
+		print("## Package %s selected! ##" % packages[0])
+		return packages[0]
+
 	print("# Please select a package:")
 	i=1
 	for package in packages:
@@ -110,14 +116,23 @@ def selectPackage(device):
 # Ask the user which file to select
 def selectFile(device, package):
 	res = cmdline("%s -s %s shell run-as %s ls databases/" % (adb, device, package))
+	
 	if (res.find("is not debuggable") != -1):
 		print("This package is not debuggale!")
 		doExit()
-	files =	makeCleanArray(res, "\n",0,["","\r"])
-	if (len(files) == 1 and files[0] == ""):
-		print("No available files")
-		doExit()
 		
+	files =	makeCleanArray(res, "\n",0,["","\r"])
+
+	if (len(files) == 1 and files[0] != ""):
+		print("No available files")
+		doExit()	
+		
+	if (len(files) == 2):
+		if (files[0].find(".db-journal")>-1):
+			del(files[0])
+		print("## file %s selected! ##" % files[0])
+		return files[0]
+	
 	newFiles = []
 	print("# Please select a file:")
 	i=1
@@ -139,46 +154,50 @@ def selectFile(device, package):
 		except:
 			print("Inavlid input, please insert a valid number!")
 	
+def mainProgram():
+	os.system(['clear','cls'][os.name == 'nt'])
+	flag = True
+	selectedDevice  = selectDevice()
+	selectedPackage = selectPackage(selectedDevice)
+	selectedFile = selectFile(selectedDevice, selectedPackage)
+	
+	print("# Press '1' to get the file")
+	print("# Press '2' to remove it from device")
+		
+	while(flag):
+		userInput = input(">> ")
+		try:
+			userInput = int(userInput)
+			if (userInput == 1): 
+				cmdline("{0} -s {1} shell run-as {2} chmod 666 databases/{3}".format(adb, selectedDevice, selectedPackage, selectedFile))
+				cmdline("%s -s %s pull /data/data/%s/databases/%s" % (adb, selectedDevice, selectedPackage, selectedFile))
+				cmdline("{0} -s {1} shell run-as {2} chmod 600 databases/{3}".format(adb, selectedDevice, selectedPackage, selectedFile))
+				flag = False
+				if (OS == 'Windows'):
+					print("Downloaded!, saved as: %s\%s" % (cmdline("echo %CD%").rstrip("\n").rstrip("\r"),selectedFile))
+				else:
+					print("Downloaded!, saved as: %s/%s" % (cmdline("pwd").rstrip("\n").rstrip("\r"),selectedFile))
+			elif (userInput == 2):
+				cmdline("{0} -s {1} shell run-as {2} rm databases/{3} databases/{3}-journal".format(adb, selectedDevice, selectedPackage, selectedFile))
+				flag = False
+				print("Removed!")
+			else:
+				print("What?! try again")
+		except:
+			if (flag == True):
+				print("Inavlid input, please insert a valid number!")
+		doExit(0)
+		mainProgram()
 	
 	
 if __name__=="__main__":
-	flag = True
-	if (os.path.exists(SDK_PATH + "platform-tools/")):
-		adb = SDK_PATH + "platform-tools/adb"
+	if (os.path.exists(SDK_PATH + "/platform-tools/")):
+		adb = SDK_PATH + "/platform-tools/adb"
 		if (OS == 'Windows'):
 			adb = adb + ".exe"
-	
-		cmdline("%s start-server" % adb)
+				
+		mainProgram();
 		
-		selectedDevice  = selectDevice()
-		selectedPackage = selectPackage(selectedDevice)
-		selectedFile = selectFile(selectedDevice, selectedPackage)
-		
-		print("# Press '1' to get the file")
-		print("# Press '2' to remove it from device")
-		
-		while(flag):
-			userInput = input(">> ")
-			try:
-				userInput = int(userInput)
-				if (userInput == 1): 
-					cmdline("{0} -s {1} shell run-as {2} chmod 666 databases/{3}".format(adb, selectedDevice, selectedPackage, selectedFile))
-					cmdline("%s -s %s pull /data/data/%s/databases/%s" % (adb, selectedDevice, selectedPackage, selectedFile))
-					cmdline("{0} -s {1} shell run-as {2} chmod 600 databases/{3}".format(adb, selectedDevice, selectedPackage, selectedFile))
-					flag = False
-					if (OS == 'Windows'):
-						print("Downloaded!, saved as: %s/%s" % (cmdline("echo %CD%").replace("\n",""),selectedFile))
-					else:
-						print("Downloaded!, saved as: %s/%s" % (cmdline("pwd").replace("\n",""),selectedFile))
-				elif (userInput == 2):
-					cmdline("{0} -s {1} shell run-as {2} rm databases/{3} databases/{3}-journal".format(adb, selectedDevice, selectedPackage, selectedFile))
-					flag = False
-					print("Removed!")
-				else:
-					print("What?! try again")
-			except:
-				print("Inavlid input, please insert a valid number!")
-			doExit()
 	else:
 		print("Please launch the 'Android SDK Manager' and download Platform Tools!")
-		doExit()
+		doExit();
